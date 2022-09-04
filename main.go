924e83be1ddef4b3f38c16c0cb6693071b4d554b
tigawanna <denniskinuthiaw@gmail.com>
2022-09-04 19:48:01 +0300

    string index

--- main.go
+++ main.go
@@ -1,27 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+)
 
 func main() {
 	var conferenceName = "Golang confernece"
 	const conferenceTickets = 50
-	var bookings []string
+	// var bookings []string
 
-	var firstName, lastName string
+	// var firstName, lastName string
 
-	for {
+	// for {
 
-		fmt.Println("welcome")
-		fmt.Println("please enter your first name")
-		fmt.Scan(&firstName)
-		fmt.Println("please enter your last name")
-		fmt.Scan(&lastName)
+	// 	fmt.Println("welcome")
+	// 	fmt.Println("please enter your first name")
+	// 	fmt.Scan(&firstName)
+	// 	fmt.Println("please enter your last name")
+	// 	fmt.Scan(&lastName)
 
-		bookings = append(bookings, firstName+" "+lastName)
+	// 	bookings = append(bookings, firstName+" "+lastName)
+        
+	// 	for index,booking := range bookings{
+    //        var names = strings.Fields(booking)
+	// 	    initial := names[0][0]
+	
+	// 	   fmt.Printf("names: %v\n", names)
+	// 	   fmt.Printf("index: %v\n", index)
+	// 	   fmt.Printf("initial: %v\n", initial)
+	// 	}
 
-		fmt.Printf("%v  welcome to %v ", bookings[len(bookings) - 1], conferenceName)
+	// 	fmt.Printf("%v  welcome to %v ", bookings[len(bookings) - 1], conferenceName)
+    //     fmt.Printf("bookings: %v\n", bookings)
 
-		fmt.Printf("bookings: %v\n", bookings)
+	// }
+ initials := conferenceName[0]
+   fmt.Printf("conferenceName: %c\n", conferenceName[0])
 
-	}
+   if (initials  == 'G'){
+     println("G matches",initials)
+   }
 }
