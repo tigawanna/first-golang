270e5009470c67b93fbfa5c48759c772f6a56735
tigawanna <denniskinuthiaw@gmail.com>
2022-09-04 19:30:27 +0300

    infinte for loop

--- main.go
+++ main.go
@@ -2,26 +2,26 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	var conferenceName = "Golang confernece"
-	const conferenceTickets  = 50
+	const conferenceTickets = 50
 	var bookings []string
 
-	var firstName , lastName string
-    
-    fmt.Println("welcome")
-	fmt.Println("please enter your first name")
-	fmt.Scan(&firstName)
-	fmt.Println("please enter your last name")
-	fmt.Scan(&lastName)
+	var firstName, lastName string
 
-    bookings = append(bookings,firstName + " " + lastName) 
+	for {
 
-	fmt.Printf("%v  welcome to %v ",bookings[0],conferenceName)
+		fmt.Println("welcome")
+		fmt.Println("please enter your first name")
+		fmt.Scan(&firstName)
+		fmt.Println("please enter your last name")
+		fmt.Scan(&lastName)
 
- 
+		bookings = append(bookings, firstName+" "+lastName)
 
-    fmt.Printf("conferenceName: %v\n", conferenceName)
-	fmt.Printf("bookings: %v\n", bookings)
+		fmt.Printf("%v  welcome to %v ", bookings[len(bookings) - 1], conferenceName)
 
+		fmt.Printf("bookings: %v\n", bookings)
+
+	}
 }
